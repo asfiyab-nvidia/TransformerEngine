b45e18c34e1494f2de47fe08ee9f77ad79b339c0
Asfiya Baig <asfiyab@nvidia.com>
2022-12-01 23:30:13 +0000

    refactor te_onnx_extensions

--- transformer_engine/pytorch/te_onnx_extensions.py
+++ transformer_engine/pytorch/te_onnx_extensions.py
@@ -24,35 +24,39 @@ def onnx_cast_from_fp8(g, input, scale_inv, fp8_tensor, itype, otype):
 @symbolic_helper.parse_args("v", "v", "v", "v", "i", "i")
 def onnx_fp8_gelu(g, input, scale, amax, scale_inv, fp8_tensor, otype):
     gelu = torch.onnx.symbolic_opset9.gelu(g, input)
-    q = g.op("TRT_FP8QuantizeLinear", gelu, scale)
-    return q
-
+    return g.op("TRT_FP8QuantizeLinear", gelu, scale_inv)
 
 @symbolic_helper.parse_args("v", "v", "i", "i", "i",
                              "v", "v", "i", "i", "i",
-                             "v", "i",
-                             "v", "v", "i", "v",
-                             "i", "i", "i")
-def onnx_fp8_gemm(g, weight, weight_scale_inverse, weight_fp8_tensor, weight_type, trans_weight,
-                      input, input_scale_inverse, input_fp8_tensor, input_type, trans_input,
-                      out, out_type,
-                      bias,
-                      pre_gelu_out,
-                      grad,
-                      workspace,
-                      workspaceSize,
-                      accumulate,
-                      use_split_accumulator):
+                             "v", "i", "v", "v", "i",
+                             "v", "i", "i", "i")
+def onnx_te_gemm(
+    g,
+    weight,
+    weight_scale_inverse,
+    weight_fp8_tensor,
+    weight_type,
+    trans_weight,
+    input,
+    input_scale_inverse,
+    input_fp8_tensor,
+    input_type,
+    trans_input,
+    out,
+    out_type,
+    bias,
+    pre_gelu_out,
+    grad,
+    workspace,
+    workspaceSize,
+    accumulate,
+    use_split_accumulator):
     if input_type == int(tex.DType.kFloat8E4M3):
-        # add DQ
         input = g.op("TRT_FP8DequantizeLinear", input, input_scale_inverse)
 
     if weight_type == int(tex.DType.kFloat8E4M3):
-        # add DQ
         weight = g.op("TRT_FP8DequantizeLinear", weight, weight_scale_inverse)
 
-    # call gemm op from onnx with trans_weight and trans_input as attributes
-    # order specified with Gemm op defined as A x B
     output = g.op("Gemm", input, weight, transA_i=trans_input, transB_i=trans_weight)
 
     empty_tensor_size = [0]
@@ -67,7 +71,7 @@ def onnx_fp8_gemm(g, weight, weight_scale_inverse, weight_fp8_tensor, weight_typ
 
 
 @symbolic_helper.parse_args("v", "v", "v", "f", "v", "v", "v",  "i")
-def onnx_layernorm_fwd_fp8(g, input, weight, bias, eps, scale, amax, scale_inv, otype):#, normalized_shape):
+def onnx_layernorm_fwd_fp8(g, input, weight, bias, eps, scale, amax, scale_inv, otype):
     normalized_shape = torch.onnx.symbolic_helper._get_tensor_sizes(input)
     if normalized_shape is None:
         ndim = torch.onnx.symbolic_helper._get_tensor_rank(input)
@@ -92,6 +96,6 @@ def onnx_layernorm_fwd_fp8(g, input, weight, bias, eps, scale, amax, scale_inv, 
 register_custom_op_symbolic('tex_ts::cast_to_fp8_ts', onnx_cast_to_fp8, OPSET)
 register_custom_op_symbolic('tex_ts::cast_from_fp8_ts', onnx_cast_from_fp8, OPSET)
 register_custom_op_symbolic('tex_ts::fp8_gelu_ts', onnx_fp8_gelu, OPSET)
-register_custom_op_symbolic('tex_ts::te_gemm_ts', onnx_fp8_gemm, OPSET)
+register_custom_op_symbolic('tex_ts::te_gemm_ts', onnx_te_gemm, OPSET)
 register_custom_op_symbolic('tex_ts::layernorm_fwd_fp8_inf_ts', onnx_layernorm_fwd_fp8, OPSET)
 
