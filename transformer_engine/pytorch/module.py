ebf781a2164d4125b48027df35c81ddbbf75a214
Asfiya Baig <asfiyab@nvidia.com>
2022-12-27 19:56:06 +0000

    fix Q/DQ scale input
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>

--- transformer_engine/pytorch/module.py
+++ transformer_engine/pytorch/module.py
@@ -615,6 +615,7 @@ class _LayerNormLinear(torch.autograd.Function):
                         ln_bias,
                         eps,
                         fp8_meta["scaling_fwd"],
+                        tex.FP8FwdTensors.GEMM1_INPUT,
                         fp8_dtype_forward,
                     )
             else:
@@ -1805,6 +1806,7 @@ class _LayerNormMLP(torch.autograd.Function):
                         ln_bias,
                         eps,
                         fp8_meta["scaling_fwd"],
+                        tex.FP8FwdTensors.GEMM1_INPUT,
                         fp8_dtype_forward,
                     )
             else:
